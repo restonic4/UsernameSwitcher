b1e376e842d93db5f83f2de6954b7069cf82689f
restonic4 <marcosruizlerma@gmail.com>
2024-11-09 12:16:37 +0100

    Gradle task

--- src/main/java/com/restonic4/serverdaw/ServerDaw.java
+++ src/main/java/com/restonic4/serverdaw/ServerDaw.java
@@ -1,22 +1,32 @@
 package com.restonic4.serverdaw;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.fabricmc.api.ModInitializer;
 
 import javax.swing.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ServerDaw implements ModInitializer {
     public static String customName = null;
 
     @Override
     public void onInitialize() {
-        System.out.println("La inyeccion de codigo esta en otra clase");
+        try {
+            String path = new File("config/username_config.json").getAbsolutePath();
+            String content = new String(Files.readAllBytes(Paths.get(path)));
+            JsonObject json = JsonParser.parseString(content).getAsJsonObject();
+            customName = json.get("username").getAsString();
+        } catch (Exception e) {
+            System.err.println("Could not read the file. " + e.getMessage());
+            customName = "Guest";
+        }
+        System.out.println("Name changed: " + customName);
     }
 
     public static String getName() {
-        if (customName == null) {
-            customName = JOptionPane.showInputDialog("Escribe tu nombre de usuario, no uses espacion o simbolos extraños: ");
-        }
-
         return customName;
     }
 }
